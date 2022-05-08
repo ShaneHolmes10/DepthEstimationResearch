fc42790ee6f910441a32252dc8c269e04cfd5cb0
ShaneHolmes10 <48338528+ShaneHolmes10@users.noreply.github.com>
2022-05-08 12:19:03 -0400

    The stereo camera is set up with the ability to extract individual images and to superimpose those images

--- Stereo camera/Camera.py
+++ Stereo camera/Camera.py
@@ -1,7 +1,22 @@
 
 import cv2
-import numpy
+import numpy as np
 
+
+def ExtractImageFromDevice(frame, index):
+    xValue = int(index*(frame.shape[1]/2))
+    return frame[0:frame.shape[0],xValue:xValue+int(frame.shape[1]/2)]
+
+def SuperImposeImages(frame1, frame2):
+    a_channel = np.ones(frame1.shape, dtype=np.float)*0.5
+    frame1 = frame1 / 255.0
+    frame1 = frame1*a_channel
+
+    a_channel = np.ones(frame1.shape, dtype=np.float)*0.5
+    frame2 = frame2 / 255.0
+    frame2 = frame2*a_channel
+
+    return frame1 + frame2
 
 # Start a video capture object from the camera device
 cap = cv2.VideoCapture(1)
@@ -11,9 +26,18 @@ while(True):
     # Get the frame from the camera device
     ret, frame = cap.read()
 
+    
+    frame1 = ExtractImageFromDevice(frame, 1)
+    frame2 = ExtractImageFromDevice(frame, 0)
+
+    frame = SuperImposeImages(frame1, frame2)
+
+    
+
+
     # Display the frame and then wait
     cv2.imshow('frame', frame)
-    cv2.waitKey(1)
+    cv2.waitKey(10)
 
     # If the window isn't visible i.e. not there then break out of loop
     if(cv2.getWindowProperty('frame', cv2.WND_PROP_VISIBLE) < 1):

d5269bd64211cbbef878c30c991c302cacc276f9
ShaneHolmes10 <48338528+ShaneHolmes10@users.noreply.github.com>
2022-05-14 10:15:36 -0400

    Still screwing around with partitioning the image into sections and doing comparison based on that

--- Stereo camera/Camera.py
+++ Stereo camera/Camera.py
@@ -2,6 +2,8 @@
 import cv2
 import numpy as np
 import ImageFuncs as imf
+from matplotlib import pyplot as plt
+import time
 
 def ExtractImageFromDevice(frame, index):
     xValue = int(index*(frame.shape[1]/2))
@@ -18,8 +20,13 @@ def SuperImposeImages(frame1, frame2):
 
     return frame1 + frame2
 
+def 
+
 # Start a video capture object from the camera device
-cap = cv2.VideoCapture(1)
+cap = cv2.VideoCapture(0)
+ret, frame = cap.read()
+
+time.sleep(5)
 
 timer = 0
 count = 0
@@ -28,30 +35,54 @@ while(True):
     # Get the frame from the camera device
     ret, frame = cap.read()
 
+
+    
     
     frame1 = ExtractImageFromDevice(frame, 1)
     frame2 = ExtractImageFromDevice(frame, 0)
+
 
     print("x shift: %s, " % count, end="")
     xshift = count
     if timer > 100:
         count += 1
     timer += 1
+    '''
     frame1 = frame1[144:144+101, 232:232+146] 
     frame2 = frame2[144:144+101, 232+xshift:232+xshift+146]
 
     print(imf.CompareImages(frame1, frame2))
 
+
+
     frame = SuperImposeImages(frame1, frame2)
+    '''
+    print(frame1.shape)
 
-
-
+    sections1 = imf.PartitionIntoSections(frame1, 75)
+    sections2 = imf.PartitionIntoSections(frame2, 75)
     
 
+    plt.rcParams['figure.dpi'] = 100
+
+    fig = plt.figure(figsize=(10, 7))
+    rows = 10
+    columns = 8
+
+    for x in range(0, 40):
+        fig.add_subplot(rows, columns, x+1)
+        plt.imshow(sections1[x])
+    
+    for x in range(0, 40):
+        fig.add_subplot(rows, columns, x+1+40)
+        plt.imshow(sections2[x])
+
+    #plt.imshow(sections1[0])
+    plt.show()
 
     # Display the frame and then wait
-    cv2.imshow('frame', frame)
-    cv2.waitKey(10)
+    #cv2.imshow('frame', sections1[0])
+    #cv2.waitKey(10)
 
     # If the window isn't visible i.e. not there then break out of loop
     if(cv2.getWindowProperty('frame', cv2.WND_PROP_VISIBLE) < 1):
--- Stereo camera/ImageFuncs.py
+++ Stereo camera/ImageFuncs.py
@@ -7,3 +7,9 @@ def CompareImages(image1, image2):
     similarity = 1 - errorL2 / (image1.shape[0] * image1.shape[1])
     return similarity
 
+def PartitionIntoSections(img, squareDim):
+  sectionsList = []
+  for x in range(squareDim, squareDim*int(img.shape[0]/squareDim)+1, squareDim):
+    for y in range(squareDim, squareDim*int(img.shape[1]/squareDim)+1, squareDim):
+      sectionsList.append(img[x-squareDim:x, y-squareDim:y])
+  return sectionsList
